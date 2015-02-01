c1598cc88f9cc885ba2c50d61661ebf52a05ad1c
JaSonS <JaSonS_toy@foxmail.com>
2015-02-01 17:15:46 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e                                                                                                                                   
+mv -f .vim ~
+mv -f .gitignore ~
+mv -f .vimrc ~
+mv -f .gitmodules ~
+mv -f README.md ~
+mv -f .git ~
+mv -f install.sh ~
+rm -rf ~/vimEnv
